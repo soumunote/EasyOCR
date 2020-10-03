1a34edf7ec7b13e0ccc29fbcd782e2b72ed037fd
Note Soumu <soumunote@gmail.com>
2020-10-03 23:44:21 +0900

    Add project files.

---
+++ ProgressWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EasyOCR
+{
+    /// <summary>
+    /// Interaction logic for ProgressWindow.xaml
+    /// </summary>
+    public partial class ProgressWindow : Window
+    {
+        public ProgressWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void doScan(string folderName)
+        {
+            var fileNames = Directory.GetFiles(folderName, "*.jpg");
+            Array.ForEach(fileNames, fileName => {
+                
+            });
+        }
+    }
+}
